3298e517a43b28ecf9055e55a3e1843985dc663a
kochengoyen77 <94593438+kochengoyen77@users.noreply.github.com>
2021-12-16 16:30:54 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -64,11 +64,16 @@ plt.show()
 right_col.pyplot(fig)
 
 #--b--
-
+list_organisasi = []
+        
 st.sidebar.header('Pengaturan Negara dengan Produksi Terbesar')
 tahun = st.sidebar.number_input("Pilih Tahun produksi", min_value=1971, max_value=2015)
 n = st.sidebar.number_input("Pilih Banyak Negara", min_value=1, max_value=None)
 
+for n in list_organisasi:
+    csv_ = csv_[csv_.kode_negara != n]
+    if n in list_kodenegarahuruf:
+        list_kodenegarahuruf.remove(n)
 dfb = df2.loc[df2['tahun'] == tahun]
 dfb = dfb.sort_values(by='produksi', ascending = False)
 df3 = dfb[:n]
