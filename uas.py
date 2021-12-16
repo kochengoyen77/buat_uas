2dc3ad258256e25615204590096e9614f1ce1198
kochengoyen77 <94593438+kochengoyen77@users.noreply.github.com>
2021-12-16 10:47:19 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -61,7 +61,7 @@ left_col.write(df2)
 
 #OUTPUT GRAFIK A
 fig, ax = plt.subplots()
-ax.bar(tahun_prod, jumlah_prod, color==colors)
+
 ax.plot(df2['tahun'], df2['produksi'], label = df2['tahun'])
 ax.set_title("Jumlah Produksi Per Tahun di Negara Pilihan")
 ax.set_xlabel("Tahun", fontsize = 12)
