5a17d7323de5eb42c391a1fd1d24d431c12b5089
kochengoyen77 <94593438+kochengoyen77@users.noreply.github.com>
2021-12-16 20:24:16 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -59,7 +59,7 @@ fig, ax = plt.subplots()
 colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
 ax.plot(df2['tahun'], df2['produksi'], label = df2['tahun'])
 ax.set_title("Jumlah Produksi Per Tahun di Negara Pilihan")
-ax.set_xlabel("Tahun", color="red", fontsize = 20)
+ax.set_xlabel("Tahun", color="green", fontsize = 20)
 ax.set_ylabel("Jumlah Produksi", color="yellow", fontsize = 20)
 ax.legend(fontsize = 20)
 plt.scatter("Tahun", "Jumlah Produksi", color="yellow", marker='x', label='item 1')
