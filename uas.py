9b9d967b2e86c8b03181e07c2e83e0f22663fba7
kochengoyen77 <94593438+kochengoyen77@users.noreply.github.com>
2021-12-16 16:08:10 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -95,7 +95,7 @@ dk = dk[:n]
 dk.plot.bar(x='kode_negara', y='kumulatif') 
 plt.show()
 st.pyplot(plt)
-
+'''
 #--d--
 #bagian 1
 jumlah_produksi = dfb[:1].iloc[0]['produksi']
@@ -220,3 +220,4 @@ dfproduksikumulatifnol['sub-region'] = listsubregionkumulatifnol
                                                         
 st.write(dfproduksinol)
 st.write(dfproduksinol)
+'''
