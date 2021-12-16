e87dd36ff5aa74f9bb434af71fd390f85b305cf5
kochengoyen77 <94593438+kochengoyen77@users.noreply.github.com>
2021-12-16 16:13:53 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -69,7 +69,7 @@ st.sidebar.header('Pengaturan Negara dengan Produksi Terbesar')
 tahun = st.sidebar.number_input("Pilih Tahun produksi", min_value=1971, max_value=2015)
 n = st.sidebar.number_input("Pilih Banyak Negara", min_value=1, max_value=None)
 
-dfb = df_info.loc[df_info['tahun'] == tahun]
+dfb = jh_.loc[jh_['tahun'] == tahun]
 dfb = dfb.sort_values(by='produksi', ascending = False)
 df3 = dfb[:n]
 print(df3)
