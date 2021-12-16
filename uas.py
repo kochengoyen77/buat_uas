f26447085f21e6b97f9d2d83c7bb16f9680dd2f6
kochengoyen77 <94593438+kochengoyen77@users.noreply.github.com>
2021-12-16 10:51:06 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -77,7 +77,7 @@ tahun = st.sidebar.number_input("Pilih Tahun produksi", min_value=1971, max_valu
 n = st.sidebar.number_input("Pilih Banyak Negara", min_value=1, max_value=None)
 
 dfb = csv_.loc[csv_['tahun'] == tahun][:n]
-dfb = dfb.sort_values(by='produksi', ascending = True)
+dfb = dfb.max_values(by='produksi', ascending = False)
 dfb = dfb[:n]
 
 dfb.plot.bar(x='kode_negara', y='produksi')
