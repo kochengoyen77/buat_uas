b138ee8b268a7bdb42b22ad2443f1c4a54e57c39
kochengoyen77 <94593438+kochengoyen77@users.noreply.github.com>
2021-12-16 17:41:45 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -23,7 +23,7 @@ print(df)
 
 
 #MEMBUAT DATA FRAME TIAP FILE
-st.title('Data Produksi Minyak Mentah')
+st.title('Data Produksi Minyak Mentah', color ='Red')
 st.header('UAS Pemrograman Komputer')
 ch_ = csvHandler('produksi_minyak_mentah.csv')
 jh_ = jsonHandler('kode_negara_lengkap.json')
