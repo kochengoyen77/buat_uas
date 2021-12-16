1b1ba6352a31cb015c2927155dc4195f1a140f05
kochengoyen77 <94593438+kochengoyen77@users.noreply.github.com>
2021-12-16 15:36:41 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -139,6 +139,49 @@ st.write("nama_negara")
 st.write("region_negara")
 st.write("subregion_negara")
 
+#bagian 2
+dfterkecil = dfb[dfb.produksi !=0]
+dfterkecil = dfterkecil.sort_values(by=['produksi'],ascending=True]
+jumlah_produksi = dfterkecil[:1].iloc[0]['produksi']
+kode_negara = dfterkecil[:1].iloc[0]['kode_negara']
+nama_negara = ""
+region_negara = ""
+subregion_negara = ""
+                                    
+for i in range(len(df_info)):
+    if list(df_info['alpha-3'])[i]==kode_negara:
+        nama_negara = list(df_info['name'])[i]
+        region_negara = list(df_info['region'])[i]
+        subregion_negara = list(df_info['sub-region'])[i]
+                                    
+st.write('Negara dengan Produksi Terkecil')
+st.write(jumlah_produksi)
+st.write(kode_negara)
+st.write(nama_negara)
+st.write(region_negara)
+st.write(subregion_negara)
+                                    
+dfkumulatifmin=dk[dk.kumulatif !=0]
+dfkumulatifmin = dfkumulatifmin[:1].sort_values(by=['produksi'], ascending = True]
+jumlah_produksi = dfkumulatifmin[:1].iloc[0]['kumulatif']
+kode_negara = dfkumulatifmin[:1].iloc[0]['kode_negara']
+nama_negara = ""
+region_negara = ""
+subregion_negara = ""
+                                                
+for i in range(len(df_info)):
+    if list(df_info['alpha-3'])[i]==kode_negara:
+        nama_negara = list(df_info['name'])[i]
+        region_negara = list(df_info['region'])[i]
+        subregion_negara = list(df_info['sub-region'])[i]
+                                                
+st.write('Negara dengan Produksi Terkecil Pada Keseluruhan Tahun')
+st.write(jumlah_produksi)
+st.write(kode_negara)
+st.write(nama_negara)
+st.write(region_negara)
+st.write(subregion_negara)
+                                                
 #d bagian 3
 dfproduksinol = dfb[dfb.produksi == 0]
 listnegaranol = []
