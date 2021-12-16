f94de7fe127ec20afa5c7f46a9bde5484012a231
kochengoyen77 <94593438+kochengoyen77@users.noreply.github.com>
2021-12-16 14:55:05 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -113,7 +113,8 @@ for i in range(len(df_info)):
 st.write('Negara dengan Produksi Terbesar')
 st.write(jumlah_produksi)
 st.write(kode_negara[:1])
-indexer = self.columns.get_loc(list(df_info['name'])[i])
+indexer = self.columns.get_loc(nama_negara)
+st.write(indexer)
 st.write(region_negara = list(df_info[region_negara])[i][0])
 st.write(subregion_negara = list(df_info['sub-region_negara'])[i][0])
 
