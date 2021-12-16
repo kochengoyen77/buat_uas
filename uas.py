d01235f6f76a6db6153a7d228eae44427b1706e2
kochengoyen77 <94593438+kochengoyen77@users.noreply.github.com>
2021-12-16 23:03:56 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -64,7 +64,7 @@ ax.set_ylabel("Jumlah Produksi", color="yellow", fontsize = 20)
 ax.legend(fontsize = 20)
 plt.scatter("Tahun", "Jumlah Produksi", color="yellow", marker='x', label='item 1')
 plt.show()
-right_col.pyplot(fig)
+left_col.pyplot(fig)
 
 #--b--
 list_kodekumpulannegara = []
