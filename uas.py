fe3cfd076b4907d6a2bf0a4e722f1ac65312ebc0
kochengoyen77 <94593438+kochengoyen77@users.noreply.github.com>
2021-12-16 10:56:54 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -76,8 +76,10 @@ st.sidebar.header('Pengaturan Negara dengan Produksi Terbesar')
 tahun = st.sidebar.number_input("Pilih Tahun produksi", min_value=1971, max_value=2015)
 n = st.sidebar.number_input("Pilih Banyak Negara", min_value=1, max_value=None)
 
-dfb = csv_.loc[csv_['tahun'] == tahun][:n]
-dfb = dfb.aort_values(by='jumlah produksi', ascending = False)
+df = df[df['tahun']==T]
+kode_negara = df[df['tahun']==T]['kode_negara'].tolist()
+dfb = csv_.loc[csv_['tahun'] == tahun]
+dfb = dfb.aort_values(by='produksi', ascending = False)
 dfb = dfb[:n]
 
 dfb.plot.bar(x='kode_negara', y='produksi')
