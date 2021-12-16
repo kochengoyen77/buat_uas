4815e8954f76193e2c79c2d9031a29241057d054
kochengoyen77 <94593438+kochengoyen77@users.noreply.github.com>
2021-12-16 10:49:36 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -75,17 +75,11 @@ right_col.pyplot(fig)
 st.sidebar.header('Pengaturan Negara dengan Produksi Terbesar')
 tahun = st.sidebar.number_input("Pilih Tahun produksi", min_value=1971, max_value=2015)
 n = st.sidebar.number_input("Pilih Banyak Negara", min_value=1, max_value=None)
-'''
-list1 = []
-for i in list1 (csv_['kode negara']):
-    if i not in (jh_['alpha-3']):
-        list1.append(i)
-for i in list1:
-    df2 = df2[df2['kode negara']!=1]
+
 dfb = csv_.loc[csv_['tahun'] == tahun][:n]
-dfb = dfb.sort_values(by='produksi', ascending = False)
+dfb = dfb.sort_values(by='produksi', ascending = True)
 dfb = dfb[:n]
-'''
+
 dfb.plot.bar(x='kode_negara', y='produksi')
 plt.show()
 st.pyplot(plt)
