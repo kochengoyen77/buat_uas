41d50caf284600e02a496c96b91b55f1ec584c09
Tan Manuel <94593438+kochengoyen77@users.noreply.github.com>
2021-12-17 10:25:36 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -78,7 +78,8 @@ for i in list(csv_['kode_negara']) :
 for i in list_kodekumpulannegara :
     csv_ = csv_[csv_.kode_negara != i]
 print(csv_)
-   
+
+st.sidebar.header('UAS PEMROGRAMAN KOMPUTER IF2112-K02)
 st.sidebar.header('Pengaturan Negara dengan Produksi Terbesar')
 tahun = st.sidebar.number_input("Pilih Tahun produksi", min_value=1971, max_value=2015)
 n = st.sidebar.number_input("Pilih Banyak Negara", min_value=1, max_value=None)
